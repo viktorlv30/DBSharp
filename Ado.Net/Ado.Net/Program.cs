a2c726841970eb7abdf096dadc7ee8d50cc9080d
viktorlv30 <viktor.lv.30@gmail.com>
2016-07-03 19:23:43 +0300

    wrote Program.cs class

--- Ado.Net/Ado.Net/Program.cs
+++ Ado.Net/Ado.Net/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,51 @@ namespace Ado.Net
     {
         static void Main(string[] args)
         {
-
+            ReadFromBase();
+            Console.ReadKey();
         }
 
-        private void ReadFromBase()
+        private static void ReadFromBase()
         {
             var conString = ConfigurationManager.AppSettings["ConnectionString"];
 
             using (var connection = new SqlConnection(conString))
             {
-                //var commandText = @SELECT DISTINCT
-                //                    e.Id
+                string commandText = @"SELECT DISTINCT
+                                      e.Id, e.Name, e.Birthday, e.Email, e.Salary
+                                    FROM
+                                      Employees e
+                                    WHERE 
+                                      LEN(e.Name) > @len";
+                var command = new SqlCommand(commandText, connection);
+                var parameter = new SqlParameter("@len", SqlDbType.Int);
+                parameter.Value = 20;
+
+                command.Parameters.Add(parameter);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    List<dynamic> dataFromDb = new List<dynamic>();
+                    while (reader.Read())
+                    {
+                        var item = new
+                        {
+                            Id = int.Parse(reader[0].ToString()),
+                            Name = reader[1].ToString(),
+                            BirthDay = reader[2].GetType() != typeof(DBNull)
+                                ? Convert.ToDateTime(reader[2])
+                                : new DateTime(),
+                            Email = reader[3].GetType() != typeof(DBNull)
+                                ? reader[3].ToString()
+                                : String.Empty,
+                            Salary = reader[4].GetType() != typeof(DBNull)
+                            ? reader[4].ToString()
+                            : String.Empty
+                        };
+                        dataFromDb.Add(item);
+                    }
+                }
             }
         }
     }
